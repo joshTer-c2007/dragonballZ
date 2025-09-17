84f808be8067cdbe4dc861b43f807741324dd614
joshTer-c2007 <jhosep.07txx@gmail.com>
2025-09-16 20:25:31 -0500

    modifica la imagen

--- ejercicio git/ejercicio.js
+++ ejercicio git/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("img.s")
     console.log("ingreasr a saludar");
+    cmpImagenSaludo.src="./imagenes/b13da3906d4f03e0f1ce818238d188a4.gif"
+
 }
