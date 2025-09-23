e050641020450a89da06f843b8a594fac07bc82e
joshTer-c2007 <jhosep.07txx@gmail.com>
2025-09-23 18:24:04 -0500

    Todo listo para subir

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -5,6 +5,11 @@ saludar=function(){
     //recuperar el valor de la caja de texto txtApellido
     let apellido;
     apellido=recuperarTexto("txtApellido");
+    
+    let edad=recuperarInt("txtEdad");
+
+    let estatura=recuperarFloat("txtEstatura");
+
 }
 recuperarTexto=function(idComponente){
     let componente;
@@ -13,3 +18,16 @@ recuperarTexto=function(idComponente){
     valorIngresado=componente.value;
     return valorIngresado;
 }
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFlotante=parseFloat(valorCaja);
+    return valorFlotante;
+}
+
