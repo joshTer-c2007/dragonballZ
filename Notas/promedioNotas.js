b51f870981e7fb6e100f1e1a5e77723c86da44b2
joshTer-c2007 <jhosep.07txx@gmail.com>
2025-09-25 16:01:43 -0500

    Lo logramos al fin

---
+++ Notas/promedioNotas.js
@@ -0,0 +1,24 @@
+calcularPromedioNotas=function(){
+    //Recuperar el valor de las cajas de texto
+    let nota1;
+    let nota2;
+    let nota3;
+    let promedio;
+    nota1=recuperarFloat("txtNota1");
+    nota2=recuperarFloat("txtNota2");
+    nota3=recuperarFloat("txtNota3");
+
+    promedio=calcularPromedio(nota1,nota2,nota3);
+
+    mostrarTexto("lblResultado",promedio);
+
+    if(promedio>7){
+        mostrarTexto("lblPalabras","Lo lograste disfruta tus vacaciones");
+        mostrarImagen("img","./imagenes/gogeta.gif");
+    }else{
+        mostrarTexto("lblPalabras","Que paso bro, no le echaste ganas :(");
+        mostrarImagen("img","./imagenes/vegito.gif");
+    }
+    
+
+}
---
+++ Notas/servicioPromedioNotas.js
@@ -0,0 +1,7 @@
+calcularPromedio=function(n1,n2,n3){
+    let suma;
+    let division;
+    suma=n1+n2+n3;
+    division=suma/3;
+    return division;
+}
